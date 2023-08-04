96e341e1ad861c4fb0d201d082f9901325edca0b
trclst <141440559+trclst@users.noreply.github.com>
2023-08-04 21:43:20 +0200

    remove libmysofa

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -16,7 +16,6 @@ ExternalProject_Add(ffmpeg
         libvpx
         libwebp
         libzimg
-        libmysofa
         fontconfig
         harfbuzz
         opus
@@ -86,7 +85,7 @@ ExternalProject_Add(ffmpeg
         --enable-libzimg
         --enable-mbedtls
         --enable-libxml2
-        --enable-libmysofa
+        --disable-libmysofa
         --enable-libssh
         --enable-libsrt
         --enable-libvpl
