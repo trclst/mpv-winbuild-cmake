cf64f1f2626b394666b10d36a8b9a92e955e3479
trclst <141440559+trclst@users.noreply.github.com>
2023-09-20 15:48:13 +0200

    fix jpeg

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -31,11 +31,12 @@ ExternalProject_Add(ffmpeg
         --disable-vapoursynth
         --disable-everything
         --enable-encoder=libx264,ac3,aac,libmp3lame,png,srt,flac,mp2
-        --enable-decoder=h264,ac3,aac,eac3,flac,mp3,png,srt,flac,mp2
+        --enable-decoder=h264,ac3,aac,eac3,flac,mp3,png,srt,flac,mp2,mjpeg
         --enable-muxer=h264,mp4,mpegts,ac3,dts,eac3,flac,matroska,mp3,srt,mp2,ogg
-        --enable-demuxer=h264,aac,ac3,ass,dts,dtshd,matroska,mp3,mpegts,srt,flac,ogg
+        --enable-demuxer=h264,aac,ac3,ass,dts,dtshd,matroska,mp3,mpegts,srt,flac,ogg,image2
         --enable-parser=h264,aac,ac3,flac,png
         --enable-protocol=file
+        --enable-zlib
         --enable-libxml2
         --enable-gmp
         --enable-libass
