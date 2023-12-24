0039aafa2a32dae593639f698eb4082fd28b9bcb
trclst <trclst@posteo.de>
2023-12-24 18:21:20 +0000

    rustup force curl to use https and tlsv1.2

--- toolchain/rustup.cmake
+++ toolchain/rustup.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(rustup
     UPDATE_COMMAND ""
     SOURCE_DIR rustup-prefix/src
     CONFIGURE_COMMAND ${EXEC} CONF=1
-        curl -sSf https://sh.rustup.rs |
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs |
         sh -s -- -y --default-toolchain stable --target x86_64-pc-windows-gnu,i686-pc-windows-gnu --no-modify-path --profile minimal
     BUILD_COMMAND ${EXEC} rustup update
     INSTALL_COMMAND ${EXEC} cargo install cargo-c --profile=release-strip --features=vendored-openssl
